bca3be285bdc930b38ab92e1e3be77589f5dcf59
kwahzee <ecrissinger97@gmail.com>
2026-02-19 15:53:39 -0500

    added blog post 1

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,8 @@
 const posts = [
   {
-    date: '',
-    title: '',
-    body: '',
+    date: 'February 19 2026',
+    title: 'new website',
+    body: "i started work on this website in early 2025 when i initially started learning to program. this year i took some time to really dive headfirst into programming and thought it would be a good project to return to and publish. i converted my 2025 html/css/javascript into a next.js (tsx) project and deployed it using vercel. i'm excited with how it turned out. while having psychedelic flashing glitch videos as background images for pages and pixle gothic font is most likely not following any web development best practices, and mobile optimization is non-existent as of now, but i'm proud of getting this far into a personal project. there are links to music i've made throughout my life, videos that i try to film and release weekly, a store where i sell music equipment i've either made or bought, a page to display programming portfolio projects, and of course, this blog where i plan on writing about programming progress and other music-related topics. i also plan on getting a comment system up and running soon for blog posts in case anyone cares to comment or ask any questions. and finally, a long-term project for this site is to program a fully interactive site-bot which might take a while, but that's one of the goals! anyways, that's all for now so thanks fer reading!",
   },
 ];
 
